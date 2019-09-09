06038ae6ec2391b6696e60096bd9de736feaf952
drakignis <a.2711@hotmail.com>
2019-09-09 09:37:46 -0500

    Updates

---
+++ precache-manifest.3241f1741ebe1ec9aa364799342e45ac.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "36cab840824e3d0a7f54f02c86f51233",
+    "url": "/pruebaGit/index.html"
+  },
+  {
+    "revision": "866a235f65acd8c0d8fc",
+    "url": "/pruebaGit/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "490696ae5f7cf136444c",
+    "url": "/pruebaGit/static/js/2.4675cc05.chunk.js"
+  },
+  {
+    "revision": "866a235f65acd8c0d8fc",
+    "url": "/pruebaGit/static/js/main.2c19e1a9.chunk.js"
+  },
+  {
+    "revision": "8406bb74299c8a54daf7",
+    "url": "/pruebaGit/static/js/runtime~main.a5a89e3f.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/pruebaGit/static/media/logo.5d5d9eef.svg"
+  }
+]);
